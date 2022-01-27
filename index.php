b1555a34d2d31f17832fd0eb906e8141c4947d2f
Mellyrisyani <97926073+Mellyrisyani@users.noreply.github.com>
2022-01-26 18:00:17 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+      <?php
+      include("koneksi.php");
+      include("konten/head.html");
+      ?>
+        <title> Document </title>
+</head>
+
+ <body>
+
+     <div class="container">
+      <?php
+      include("konten/judul.html");
+      include("konten/nav.php");
+
+      include("atur-konten.php");
+      ?>
+     </div>
